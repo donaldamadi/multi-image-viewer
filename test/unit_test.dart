ffa5fe2b5b389a88f37d63f937a676f10bc61963
Donald Amadi <donaldamadi15@gmail.com>
2023-08-21 01:40:14 +0100

    Unit and Widget Tests

---
+++ test/unit_test.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:multi_image_layout/multi_image_layout.dart';
+
+
+void main() {
+  test('ImageModel should hold imageUrl and caption correctly', () {
+    final model = ImageModel(imageUrl: 'testUrl', caption: 'testCaption');
+    expect(model.imageUrl, 'testUrl');
+    expect(model.caption, 'testCaption');
+  });
+}
+
---
+++ test/widget_test.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:multi_image_layout/multi_image_layout.dart';
+
+void main() {
+  testWidgets('GalleryPhotoViewWrapper displays image and caption', (WidgetTester tester) async {
+    const mockImageUrl = 'assets/images/simple_image.jpeg';
+    await tester.pumpWidget(MaterialApp(
+      home: GalleryPhotoViewWrapper(
+        galleryItems: const [mockImageUrl],
+        captions: const ['Test caption'],
+        initialIndex: 0,
+      ),
+    ));
+
+    expect(find.byType(PhotoViewGallery), findsOneWidget);
+    expect(find.text('Test caption'), findsOneWidget);
+  });
+}
