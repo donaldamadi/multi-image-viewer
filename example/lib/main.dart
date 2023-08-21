ddedaa7722431cfe440d77c6f17d7496ed453ab4
Donald Amadi <donaldamadi15@gmail.com>
2023-08-21 01:41:45 +0100

    Updated Example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:multi_image_layout/multi_image_layout.dart';
 
 void main() {
@@ -42,71 +41,78 @@ class _MyHomePageState extends State<MyHomePage> {
           padding: const EdgeInsets.all(16.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
-            children: const [
-              Text("Single(1) Image"),
-              SizedBox(height: 10),
+            children: [
+              const Text("Single(1) Image"),
+              const SizedBox(height: 10),
+              MultiImageViewer(images: [
+                ImageModel(
+                  imageUrl: "https://3.img-dpreview.com/files/p/TS250x250~sample_galleries/3800753625/4086993630.jpg",
+                  caption:
+                      "Ut eu pariatur sit cupidatat ut sit enim do enim eiusmod officia enim consequat fugiat. Est amet tempor duis tempor sunt reprehenderit officia ut est cillum tempor. Cupidatat sunt nostrud laboris exercitation. Officia do eu consectetur officia velit in nulla ullamco qui elit laborum anim. Do dolore magna id adipisicing excepteur in qui. Irure quis pariatur amet in.",
+                )
+              ]),
+              const SizedBox(height: 30),
+              const Text("Two(2) Images"),
+              const SizedBox(height: 10),
               MultiImageViewer(
                 images: [
-                  "https://3.img-dpreview.com/files/p/TS250x250~sample_galleries/3800753625/4086993630.jpg",
-                ],
-                captions: [
-                  "Writing a very long text to check if the feature works as expected. This is a caption for the image. I hope it works as expected.",
+                  ImageModel(
+                    imageUrl: "https://4.img-dpreview.com/files/p/TS250x250~sample_galleries/3800753625/4684313123.jpg",
+                    caption: "Caption 1",
+                  ),
+                  ImageModel(
+                    imageUrl: "https://3.img-dpreview.com/files/p/TS250x250~sample_galleries/3800753625/8719688791.jpg",
+                    caption: "Caption 2",
+                  ),
                 ],
               ),
-              SizedBox(height: 30),
-              Text("Two(2) Images"),
-              SizedBox(height: 10),
+              const SizedBox(height: 30),
+              const Text("Three(3) Images"),
+              const SizedBox(height: 10),
               MultiImageViewer(
                 images: [
-                  "https://4.img-dpreview.com/files/p/TS250x250~sample_galleries/3800753625/4684313123.jpg",
-                  "https://3.img-dpreview.com/files/p/TS250x250~sample_galleries/3800753625/8719688791.jpg",
-                ],
-                captions: [
-                  "Writing a very long text to check if the feature works as expected. This is a caption for the image. I hope it works as expected.",
-                  "Caption 2",
+                  ImageModel(
+                      imageUrl: "https://3.img-dpreview.com/files/p/TS250x250~sample_galleries/3800753625/8719688791.jpg", caption: "A caption here"),
+                  ImageModel(imageUrl: "https://3.img-dpreview.com/files/p/TS250x250~sample_galleries/3800753625/4086993630.jpg"),
+                  ImageModel(
+                      imageUrl: "https://2.img-dpreview.com/files/p/TS250x250~sample_galleries/3800753625/5713335642.jpg",
+                      caption: "Another caption here"),
                 ],
               ),
-              SizedBox(height: 30),
-              Text("Three(3) Images"),
-              SizedBox(height: 10),
+              const SizedBox(height: 30),
+              const Text("Four(4) Images"),
+              const SizedBox(height: 10),
               MultiImageViewer(
                 images: [
-                  "https://3.img-dpreview.com/files/p/TS250x250~sample_galleries/3800753625/8719688791.jpg",
-                  "https://3.img-dpreview.com/files/p/TS250x250~sample_galleries/3800753625/4086993630.jpg",
-                  "https://2.img-dpreview.com/files/p/TS250x250~sample_galleries/3800753625/5713335642.jpg",
+                  ImageModel(imageUrl: "https://3.img-dpreview.com/files/p/TS250x250~sample_galleries/3800753625/8719688791.jpg"),
+                  ImageModel(imageUrl: "https://3.img-dpreview.com/files/p/TS250x250~sample_galleries/3800753625/4086993630.jpg"),
+                  ImageModel(imageUrl: "https://2.img-dpreview.com/files/p/TS250x250~sample_galleries/3800753625/5713335642.jpg"),
+                  ImageModel(imageUrl: "https://1.img-dpreview.com/files/p/TS250x250~sample_galleries/3800753625/6294874831.jpg"),
                 ],
               ),
-              SizedBox(height: 30),
-              Text("Four(4) Images"),
-              SizedBox(height: 10),
+              const SizedBox(height: 30),
+              const Text("More than Four(4+) Images"),
+              const SizedBox(height: 10),
               MultiImageViewer(
                 images: [
-                  "https://3.img-dpreview.com/files/p/TS250x250~sample_galleries/3800753625/8719688791.jpg",
-                  "https://3.img-dpreview.com/files/p/TS250x250~sample_galleries/3800753625/4086993630.jpg",
-                  "https://2.img-dpreview.com/files/p/TS250x250~sample_galleries/3800753625/5713335642.jpg",
-                  "https://1.img-dpreview.com/files/p/TS250x250~sample_galleries/3800753625/6294874831.jpg"
+                  ImageModel(
+                    imageUrl: "https://3.img-dpreview.com/files/p/TS250x250~sample_galleries/3800753625/8719688791.jpg",
+                    caption: "A caption here",
+                  ),
+                  ImageModel(imageUrl: "https://3.img-dpreview.com/files/p/TS250x250~sample_galleries/3800753625/4086993630.jpg"),
+                  ImageModel(imageUrl: "https://2.img-dpreview.com/files/p/TS250x250~sample_galleries/3800753625/5713335642.jpg"),
+                  ImageModel(imageUrl: "https://1.img-dpreview.com/files/p/TS250x250~sample_galleries/3800753625/6294874831.jpg"),
+                  ImageModel(imageUrl: "https://3.img-dpreview.com/files/p/TS250x250~sample_galleries/3800753625/8719688791.jpg"),
+                  ImageModel(imageUrl: "https://3.img-dpreview.com/files/p/TS250x250~sample_galleries/3800753625/4086993630.jpg"),
+                  ImageModel(imageUrl: "https://2.img-dpreview.com/files/p/TS250x250~sample_galleries/3800753625/5713335642.jpg"),
                 ],
-              ),
-              SizedBox(height: 30),
-              Text("More than Four(4+) Images"),
-              SizedBox(height: 10),
-              MultiImageViewer(
-                images: [
-                  "https://3.img-dpreview.com/files/p/TS250x250~sample_galleries/3800753625/8719688791.jpg",
-                  "https://3.img-dpreview.com/files/p/TS250x250~sample_galleries/3800753625/4086993630.jpg",
-                  "https://2.img-dpreview.com/files/p/TS250x250~sample_galleries/3800753625/5713335642.jpg",
-                  "https://1.img-dpreview.com/files/p/TS250x250~sample_galleries/3800753625/6294874831.jpg",
-                  "https://3.img-dpreview.com/files/p/TS250x250~sample_galleries/3800753625/8719688791.jpg",
-                  "https://3.img-dpreview.com/files/p/TS250x250~sample_galleries/3800753625/4086993630.jpg",
-                  "https://2.img-dpreview.com/files/p/TS250x250~sample_galleries/3800753625/5713335642.jpg",
-                ],
-                textStyle: TextStyle(
+                textStyle: const TextStyle(
                   fontWeight: FontWeight.bold,
-                  color: Colors.red,
                   fontSize: 23,
+                  color: Colors.white,
                 ),
               ),
-              SizedBox(height: 30),
+              const SizedBox(height: 30),
             ],
           ),
         ),
