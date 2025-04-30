a9d27ffeab61c611b074bdddbeec44b7c9a87b28
Majid <majeedabdul.941485@gmail.com>
2025-04-30 15:02:53 +0500

    Recreated example flutter app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -5,23 +5,21 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue),
       home: const MyHomePage(title: 'Multi Image Layout Example'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  const MyHomePage({super.key, required this.title});
 
   final String title;
 
@@ -33,9 +31,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
+      appBar: AppBar(title: Text(widget.title)),
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
@@ -44,14 +40,16 @@ class _MyHomePageState extends State<MyHomePage> {
             children: [
               const Text("Single(1) Image"),
               const SizedBox(height: 10),
-              MultiImageViewer(images: [
-                ImageModel(
-                  imageUrl:
-                      "https://3.img-dpreview.com/files/p/TS250x250~sample_galleries/3800753625/4086993630.jpg",
-                  caption:
-                      "Ut eu pariatur sit cupidatat ut sit enim do enim eiusmod officia enim consequat fugiat. Est amet tempor duis tempor sunt reprehenderit officia ut est cillum tempor. Cupidatat sunt nostrud laboris exercitation. Officia do eu consectetur officia velit in nulla ullamco qui elit laborum anim. Do dolore magna id adipisicing excepteur in qui. Irure quis pariatur amet in.",
-                )
-              ]),
+              MultiImageViewer(
+                images: [
+                  ImageModel(
+                    imageUrl:
+                        "https://3.img-dpreview.com/files/p/TS250x250~sample_galleries/3800753625/4086993630.jpg",
+                    caption:
+                        "Ut eu pariatur sit cupidatat ut sit enim do enim eiusmod officia enim consequat fugiat. Est amet tempor duis tempor sunt reprehenderit officia ut est cillum tempor. Cupidatat sunt nostrud laboris exercitation. Officia do eu consectetur officia velit in nulla ullamco qui elit laborum anim. Do dolore magna id adipisicing excepteur in qui. Irure quis pariatur amet in.",
+                  ),
+                ],
+              ),
               const SizedBox(height: 30),
               const Text("Two(2) Images"),
               const SizedBox(height: 10),
@@ -75,16 +73,19 @@ class _MyHomePageState extends State<MyHomePage> {
               MultiImageViewer(
                 images: [
                   ImageModel(
-                      imageUrl:
-                          "https://3.img-dpreview.com/files/p/TS250x250~sample_galleries/3800753625/8719688791.jpg",
-                      caption: "A caption here"),
+                    imageUrl:
+                        "https://3.img-dpreview.com/files/p/TS250x250~sample_galleries/3800753625/8719688791.jpg",
+                    caption: "A caption here",
+                  ),
                   ImageModel(
-                      imageUrl:
-                          "https://3.img-dpreview.com/files/p/TS250x250~sample_galleries/3800753625/4086993630.jpg"),
+                    imageUrl:
+                        "https://3.img-dpreview.com/files/p/TS250x250~sample_galleries/3800753625/4086993630.jpg",
+                  ),
                   ImageModel(
-                      imageUrl:
-                          "https://2.img-dpreview.com/files/p/TS250x250~sample_galleries/3800753625/5713335642.jpg",
-                      caption: "Another caption here"),
+                    imageUrl:
+                        "https://2.img-dpreview.com/files/p/TS250x250~sample_galleries/3800753625/5713335642.jpg",
+                    caption: "Another caption here",
+                  ),
                 ],
               ),
               const SizedBox(height: 30),
@@ -93,17 +94,21 @@ class _MyHomePageState extends State<MyHomePage> {
               MultiImageViewer(
                 images: [
                   ImageModel(
-                      imageUrl:
-                          "https://3.img-dpreview.com/files/p/TS250x250~sample_galleries/3800753625/8719688791.jpg"),
+                    imageUrl:
+                        "https://3.img-dpreview.com/files/p/TS250x250~sample_galleries/3800753625/8719688791.jpg",
+                  ),
                   ImageModel(
-                      imageUrl:
-                          "https://3.img-dpreview.com/files/p/TS250x250~sample_galleries/3800753625/4086993630.jpg"),
+                    imageUrl:
+                        "https://3.img-dpreview.com/files/p/TS250x250~sample_galleries/3800753625/4086993630.jpg",
+                  ),
                   ImageModel(
-                      imageUrl:
-                          "https://2.img-dpreview.com/files/p/TS250x250~sample_galleries/3800753625/5713335642.jpg"),
+                    imageUrl:
+                        "https://2.img-dpreview.com/files/p/TS250x250~sample_galleries/3800753625/5713335642.jpg",
+                  ),
                   ImageModel(
-                      imageUrl:
-                          "https://1.img-dpreview.com/files/p/TS250x250~sample_galleries/3800753625/6294874831.jpg"),
+                    imageUrl:
+                        "https://1.img-dpreview.com/files/p/TS250x250~sample_galleries/3800753625/6294874831.jpg",
+                  ),
                 ],
               ),
               const SizedBox(height: 30),
@@ -117,23 +122,29 @@ class _MyHomePageState extends State<MyHomePage> {
                     caption: "A caption here",
                   ),
                   ImageModel(
-                      imageUrl:
-                          "https://3.img-dpreview.com/files/p/TS250x250~sample_galleries/3800753625/4086993630.jpg"),
+                    imageUrl:
+                        "https://3.img-dpreview.com/files/p/TS250x250~sample_galleries/3800753625/4086993630.jpg",
+                  ),
                   ImageModel(
-                      imageUrl:
-                          "https://2.img-dpreview.com/files/p/TS250x250~sample_galleries/3800753625/5713335642.jpg"),
+                    imageUrl:
+                        "https://2.img-dpreview.com/files/p/TS250x250~sample_galleries/3800753625/5713335642.jpg",
+                  ),
                   ImageModel(
-                      imageUrl:
-                          "https://1.img-dpreview.com/files/p/TS250x250~sample_galleries/3800753625/6294874831.jpg"),
+                    imageUrl:
+                        "https://1.img-dpreview.com/files/p/TS250x250~sample_galleries/3800753625/6294874831.jpg",
+                  ),
                   ImageModel(
-                      imageUrl:
-                          "https://3.img-dpreview.com/files/p/TS250x250~sample_galleries/3800753625/8719688791.jpg"),
+                    imageUrl:
+                        "https://3.img-dpreview.com/files/p/TS250x250~sample_galleries/3800753625/8719688791.jpg",
+                  ),
                   ImageModel(
-                      imageUrl:
-                          "https://3.img-dpreview.com/files/p/TS250x250~sample_galleries/3800753625/4086993630.jpg"),
+                    imageUrl:
+                        "https://3.img-dpreview.com/files/p/TS250x250~sample_galleries/3800753625/4086993630.jpg",
+                  ),
                   ImageModel(
-                      imageUrl:
-                          "https://2.img-dpreview.com/files/p/TS250x250~sample_galleries/3800753625/5713335642.jpg"),
+                    imageUrl:
+                        "https://2.img-dpreview.com/files/p/TS250x250~sample_galleries/3800753625/5713335642.jpg",
+                  ),
                 ],
                 textStyle: const TextStyle(
                   fontWeight: FontWeight.bold,
